34db04f461318a2b581489662b87a479ef79db77
Pipdoodle13 <107085381+Pipdoodle13@users.noreply.github.com>
2023-10-20 20:11:04 -0400

    Vertical mirror

--- Scripts/MirrorShadow.gd
+++ Scripts/MirrorShadow.gd
@@ -3,26 +3,33 @@ extends CharacterBody2D
 var moveSpeed : float = 100.0
 var jumpForce : float = 400.0
 var gravity : float = 600.0
+var grounded : float = true
 var paused : bool = false
 @onready var sprite : Sprite2D = $Sprite
 @onready var ap : AnimationPlayer = $AnimationPlayer
 	
 func _physics_process(delta):
 	if not paused:
-		if not is_on_floor():
-			velocity.y += gravity * delta
+		if not grounded:
+			velocity.y -= gravity * delta
 			
 		velocity.x = 0
 		if Input.is_action_pressed("moveLeft"):
 			velocity.x += moveSpeed
-			sprite.flip_h = true
+			sprite.flip_h = false
 		if Input.is_action_pressed("moveRight"):
 			velocity.x -= moveSpeed
-			sprite.flip_h = false
-		if Input.is_action_pressed("jump") and is_on_floor():
-			velocity.y = -jumpForce
+			sprite.flip_h = true
+		if Input.is_action_pressed("jump") and grounded:
+			velocity.y += jumpForce
 			
 		move_and_slide()
+		grounded = false
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			if collision.get_collider().is_in_group("Platform"):
+				grounded = true
+		
 		updateAnimation()
 	
 func updateAnimation():
