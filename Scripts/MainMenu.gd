650dbbe562756c05ccc39872547dede44005410a
bramblestars <txxcui@gmail.com>
2023-10-21 23:41:45 -0500

    change main menu to load level 1 light

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
-@export_file("*.tscn") var startScene
+@onready var startScenePath = "res://Scenes/Levels/level_1_light.tscn"
 @export var creditsText : MarginContainer
 @export var menuButtons : MarginContainer
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file(startScene)
+	get_tree().change_scene_to_file(startScenePath)
 
 func _on_quit_pressed():
 	get_tree().quit()
